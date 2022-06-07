7238e198e25692c4b3a7421ffbd6333ab0c57718
babka40000 <it-master@list.ru>
2022-06-07 22:00:18 +0400

    init

---
+++ application/db/people.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+
+def get_employees():
+    print('Выводим список служащих')
+    current_date = date.today()
+    print('Текущая дата:', current_date)
---
+++ application/salary.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+
+def calculate_salary():
+    print('Расчитываем зарплату')
+    current_date = date.today()
+    print('Текущая дата:', current_date)\
